87b355c5755662d632d370b5c12e7feab3909c24
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-04-17 12:01:09 -0700

    Add navbar, container, navbar brand and text for workshop readiness below navbar to prepare for weekend workshop assignment.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='Nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
