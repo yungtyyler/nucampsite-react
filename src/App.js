6743f91a4dce727053297d98c654adfb87d17a50
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-04-29 13:05:08 -0700

    Add aboutPage, partner folder with partner.js, partnersList.js, partnersSlice.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
-
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
       <Routes>
         <Route path='/' element={ <HomePage /> } />
         <Route path='contact' element={ <ContactPage/> } />
+        <Route path='about' element={ <AboutPage /> } />
         <Route path='directory' element={ <CampsitesDirectoryPage/> } />
-        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='directory/:campsiteId' element={ <CampsiteDetailPage /> } />
       </Routes>
       <Footer />
     </div>
