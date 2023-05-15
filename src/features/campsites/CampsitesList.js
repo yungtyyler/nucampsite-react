ac22b42b973dbbd0300e8fd56cb1c2374da03c53
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-05-15 09:40:25 -0700

    Error and loading

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,10 +2,31 @@ import { useSelector } from "react-redux";
 import CampsiteCard from "./CampsiteCard";
 import { Col, Row } from 'reactstrap';
 import { selectAllCampsites } from './campsitesSlice';
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const CampsitesList = () => {
     const campsites = useSelector(selectAllCampsites);
     console.log(campsites);
+
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    if(isLoading) {
+        return(
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if(errMsg) {
+        return(
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    };
 
     return (
         <Row className="ms-auto">
